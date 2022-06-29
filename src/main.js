1b6bd1c6d1da92988c7fcc0d958b92a0f6d823d9
kajuga <msg2fedorov@gmail.com>
2022-06-29 12:17:24 +0300

    Build the UI for Category API: Adding a Category & add router, Axios and Sweet alert integration,

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import swal from 'sweetalert';
+
+window.axios = require('axios')
+window.Swal = swal
 
 createApp(App).use(router).mount('#app')
