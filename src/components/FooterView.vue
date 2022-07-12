81254fd25960785ac920dcf55fdbd9847e89ac90
kajuga <msg2fedorov@gmail.com>
2022-07-12 18:07:19 +0300

    Небольшие правки homepage и страницы выбора товара

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -6,17 +6,13 @@
           <ul style="list-style-type: none">
             <li className="text-light font-weight-bold pb-2">Get to Know Us</li>
             <li><a className="footer-link font-weight-light" href="#">About Us</a></li>
-            <li><a className="footer-link font-weight-light" href="#">Android App</a><br></li>
-            <li><a className="footer-link font-weight-light" href="#">iOS App</a><br></li>
           </ul>
         </div>
 
         <div className="col-md-3 col-6">
           <ul style="list-style-type: none">
             <li className="text-light font-weight-bold">Connect With Us</li>
-            <li><a className="footer-link font-weight-light" href="#">Facebook</a></li>
-            <li><a className="footer-link font-weight-light" href="#">Twitter</a><br></li>
-            <li><a className="footer-link font-weight-light" href="#">Instagram</a><br></li>
+            <li><a className="footer-link font-weight-light" href="#">Telegramm</a><br></li>
           </ul>
         </div>
 
@@ -24,18 +20,13 @@
           <ul style="list-style-type: none">
             <li className="text-light font-weight-bold">Make Money With Us</li>
             <li><a className="footer-link font-weight-light" href="#">Sell with Us</a></li>
-            <li><a className="footer-link font-weight-light" href="#">Become an Affiliate</a><br></li>
-            <li><a className="footer-link font-weight-light" href="#">Advertise Your Products</a><br></li>
           </ul>
         </div>
 
         <div className="col-md-3 col-6">
           <ul style="list-style-type: none">
             <li className="text-light font-weight-bold">Let Us Help You</li>
-            <li><a className="footer-link font-weight-light" href="#">Return Centre</a></li>
-            <li><a className="footer-link font-weight-light" href="#">100% Purchase Protection</a><br></li>
             <li><a className="footer-link font-weight-light" href="#">Help</a><br></li>
-            <li><a className="footer-link font-weight-light" href="#">App Download</a><br></li>
           </ul>
         </div>
       </div>
